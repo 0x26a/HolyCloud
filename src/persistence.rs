4959327c8d5745db5a23f2d87c3f5879ff36c8f1
0x26a <65681383+0x26a@users.noreply.github.com>
2024-11-01 22:48:37 +0000

    Add files via upload

---
+++ src/persistence.rs
@@ -0,0 +1,165 @@
+use crate::topology::*;
+use crate::topology::homology::Homology;
+use kiddo::KdTree;
+use kiddo::Manhattan;
+use core::array::from_fn;
+
+
+pub struct VietorisRips<const D: usize, const T: usize>{
+    ε: f64,
+    pts: usize,
+    tree: KdTree<f64,D>,
+    skeleton: Vec<Vec<bool>>,
+    raw_complex: Vec<Simplex>,
+}
+impl<const D: usize, const T: usize> VietorisRips<D,T>{
+    pub fn init(cloud: Vec<[f64;D]>) -> VietorisRips<{D},{T}>{
+        assert!(D <= 3 && D >= 2,"supported dimensions: 2, 3.");
+        let mut i = 0;
+        let pts = cloud.len();
+
+        let mut tree: KdTree<f64, {D}> = KdTree::new();
+        let skeleton: Vec<Vec<bool>> = vec![vec![false;pts];pts];
+        let mut raw_complex: Vec<Simplex> = Vec::new();
+
+        for array in cloud.iter(){
+            tree.add(&array,(pts - i - 1) as u64);
+            raw_complex.push(Simplex{
+                value: vec![i as usize],
+                dimension: 0
+            });
+            i += 1;
+        }
+        VietorisRips{
+            ε: 0.0,
+            pts,
+            tree,
+            skeleton,
+            raw_complex
+        }
+    }
+    pub fn add_2_simplex(&mut self, triangles: &mut Vec<[usize;3]>) -> bool{
+        let mut added_2_simplex = false;
+        for a0 in 0..self.pts{
+            for a1 in a0..self.pts{
+                if self.skeleton[a0][a1]{
+                    for a2 in a1..self.pts{
+                        if self.skeleton[a1][a2] && self.skeleton[a0][a2] && !triangles.contains(&[a0,a1,a2]){
+                            // a0 - a1 - a2
+                            triangles.push([a0,a1,a2]);
+                            self.raw_complex.push(Simplex{
+                                value: vec![a0,a1,a2],
+                                dimension: 2
+                            });
+                            added_2_simplex = true;
+                            //println!("~ ( ε = {} ) 2-simplex {}-{}-{} ", self.ε, a0, a1, a2);
+                        }
+                    }
+                }
+            }
+        }
+        added_2_simplex
+    }
+    pub fn add_3_simplex(&mut self, tetrahedrons: &mut Vec<[usize;4]>){
+        for a0 in 0..self.pts{
+            for a1 in a0..self.pts{
+                if self.skeleton[a0][a1]{
+                    for a2 in a1..self.pts{
+                        if self.skeleton[a1][a2]{
+                            for a3 in a2..self.pts{
+                                if self.skeleton[a0][a2] && self.skeleton[a0][a3] && self.skeleton[a1][a3] && self.skeleton[a2][a3] && !tetrahedrons.contains(&[a0,a1,a2,a3]){
+                                    tetrahedrons.push([a0,a1,a2,a3]);
+                                    self.raw_complex.push(Simplex{
+                                        value: vec![a0,a1,a2,a3],
+                                        dimension: 3
+                                    });
+                                    //println!("~ ( ε = {} ) 3-simplex {}-{}-{}-{}", self.ε, a0, a1, a2, a3);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    pub fn analyze(&mut self, ring: Ring, end: f64, step: f64) -> Vec<Homology<{T}>>{
+
+        let mut first_iteration = true;
+        let mut added_1_simplex = false;
+        let mut triangles: Vec<[usize;3]> = Vec::new();
+        let mut tetrahedrons: Vec<[usize;4]> = Vec::new();
+
+        let mut current_homology = Homology{
+            start: 0.0,
+            end: 0.0,
+            ranks: [0;T],
+            torsions: from_fn(|_| vec![])
+        };
+        let mut data: Vec<Homology<T>> = Vec::new();
+
+        self.ε = 0.0;
+        while self.ε <= end{
+            println!("ε={}", self.ε);
+            for (i,pos) in self.tree.iter(){
+                let nbhd = self.tree.within::<Manhattan>(&pos,self.ε);
+                for point in nbhd{
+                    if point.item > i && point.distance >= self.ε - step{
+                        self.raw_complex.push(Simplex{
+                            value: vec![i as usize,point.item as usize],
+                            dimension: 1
+                        });
+
+                        self.skeleton[i as usize][point.item as usize] = true;
+                        self.skeleton[point.item as usize][i as usize] = true;
+
+                        added_1_simplex = true;
+                        //println!("~ ( ε = {} ) 1-simplex {}-{}", self.ε, i, point.item);
+                    }
+                }
+            }
+
+            if added_1_simplex{ 
+                if self.add_2_simplex(&mut triangles) && D == 3{
+                    self.add_3_simplex(&mut tetrahedrons);
+                }
+            }
+            if added_1_simplex || first_iteration{
+                self.update_homology(SimplicialComplex::from(self.raw_complex.clone(), ring.clone()), &mut current_homology, &mut data, first_iteration);
+            }
+
+            first_iteration = false;
+            added_1_simplex = false;
+            self.ε += step;
+        }
+        data.push(Homology{
+            start: current_homology.start,
+            end: self.ε,
+            ranks: current_homology.ranks,
+            torsions: current_homology.torsions.clone()
+        });
+        data
+    }
+
+    pub fn update_homology(&mut self, vr: SimplicialComplex, current_homology: &mut Homology<T>, data: &mut Vec<Homology<T>>, first_iteration: bool){
+        let ranks_and_torsions: [(usize,Torsion);T] = from_fn(|k| vr.H(k));
+        let new_ranks = ranks_and_torsions.clone().map(|c| c.0);
+        let new_torsions = ranks_and_torsions.map(|c| c.1);
+        if first_iteration{
+            current_homology.start = self.ε;
+            current_homology.ranks = new_ranks;
+            current_homology.torsions = new_torsions.clone();
+        }
+        if current_homology.ranks != new_ranks || current_homology.torsions != new_torsions{
+
+            data.push(Homology{
+                start: current_homology.start,
+                end: self.ε,
+                ranks: current_homology.ranks,
+                torsions: current_homology.torsions.clone()
+            });
+            current_homology.start = self.ε;
+            current_homology.ranks = new_ranks;
+            current_homology.torsions = new_torsions;
+        }
+    }
+}
---
+++ src/topology.rs
@@ -0,0 +1,125 @@
+pub mod samples;
+mod display;
+pub mod homology;
+mod smith_normal_form;
+
+use nalgebra::DMatrix;
+
+const TOL: f64 = 0.000001;
+
+#[derive(Clone)]
+#[derive(Debug)]
+#[derive(PartialEq)]
+pub enum Ring{
+	TRIVIAL,
+	R, // R
+	Z, // Z
+}
+pub type Rank = usize;
+pub type Torsion = Vec<usize>;
+
+#[derive(Clone)]
+#[derive(Debug)]
+pub struct const_Simplex<'a>{
+	pub value: &'a [usize],
+	pub dimension: usize
+}
+#[derive(Clone)]
+#[derive(Debug)]
+pub struct Simplex{
+	pub value: Vec<usize>,
+	pub dimension: usize
+}
+
+#[derive(Debug)]
+pub struct SimplicialComplex{
+	pub ring: Ring,
+	simplicies : Vec<Simplex>,
+	pub dimension: usize,
+	chains: Vec<Vec<Simplex>> //chains[n] = basis of A free module C_n
+
+}
+
+impl Simplex{
+	pub fn from(value: Vec<usize>) -> Simplex{ let l = value.len(); Simplex{ value: value, dimension: l - 1 } }
+}
+
+impl SimplicialComplex{
+	pub fn from(simplicies: Vec<Simplex>, ring: Ring) -> SimplicialComplex{ 
+		let mut s = SimplicialComplex{ ring, simplicies, dimension: 0, chains: vec![] };
+		s.gen_C_spaces();
+		return s;
+	}	
+	pub fn const_from(const_simplicies: &[&const_Simplex], ring: Ring) -> SimplicialComplex{
+		let mut simplicies: Vec<Simplex> = Vec::new();
+		for simp in const_simplicies{ simplicies.push(Simplex{ value: simp.value.to_vec(), dimension: simp.dimension}); }
+		let mut s = SimplicialComplex{ ring, simplicies, dimension: 0, chains: vec![] };
+		s.gen_C_spaces();
+		return s;
+	}
+
+	fn gen_C_spaces(&mut self){
+		let mut l = 0;
+		let mut dimension = 0;
+		let mut chains: Vec<Vec<Simplex>> = vec![vec![]];
+		for f in &self.simplicies{
+			while l < f.dimension{
+				chains.push(vec![]);
+				l += 1;
+			}
+			chains[f.dimension].push(f.clone());
+			dimension = f.dimension;
+		}
+		self.dimension = dimension;
+		self.chains = chains;
+	}
+
+
+	pub fn dim_C(&self, n: usize) -> usize{ self.chains[n].len() }
+
+	pub fn boundary(&self, n: usize) -> DMatrix<f64>{
+		match n{
+			
+			n if n == 0 => DMatrix::from_vec(1,self.chains[0].len(),vec![0.0;self.chains[0].len()]),
+
+			n if n > self.dimension => DMatrix::from_vec(1,1,vec![0.0]),
+
+			_ => {
+				let p = self.dim_C(n);
+				let q = self.dim_C(n-1);
+				let mut vec_of_mat: Vec<f64> = Vec::new();
+				for simplex in self.chains[n].iter(){
+					let mut coord = self.boundary_basis(n, q, simplex);
+					// println!("∂({}) = {:?}", simplex.value, coord);
+					vec_of_mat.append(&mut coord);
+				}
+				DMatrix::from_vec(q,p,vec_of_mat)
+			}
+		}
+	}
+	fn boundary_basis(&self, n: usize, p: usize, simplex: &Simplex) -> Vec<f64>{
+
+		let mut position = 0;
+		let mut vector = vec![0.0;p];
+		let mut value = simplex.value.clone();
+		let mut i = 0;
+		let l = value.len();
+
+		while i < l{
+			value.remove(i);
+			for s in &self.chains[n-1]{
+				//println!("{:?} == {:?}", s.value, value);
+				if s.value == value{ break; }
+				position += 1
+			}
+			vector[position] = if i % 2 == 0 { 1.0 } else { -1.0 };
+			value = simplex.value.clone();
+			position = 0;
+			i += 1;
+		}
+		vector
+	}
+
+
+}
+
